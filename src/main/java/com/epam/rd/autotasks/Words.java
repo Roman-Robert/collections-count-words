e5877882d787e66f26e273712f75dc495babd1a5
Roman-Robert <116169632+Roman-Robert@users.noreply.github.com>
2023-03-08 00:01:42 +0200

    change list to map, more clear return for the second task

--- src/main/java/com/epam/rd/autotasks/Words.java
+++ src/main/java/com/epam/rd/autotasks/Words.java
@@ -1,45 +1,32 @@
 package com.epam.rd.autotasks;
 
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Words {
 
-/*
-    public static void main(String[] args) {
-        String string1 = " Roma Roma roma Roma Roma Roma Roma Roma Roma Roma Roma roMA";
-        String string2 = " One1 one1 one1 one1 one1 one1 one1 one1 one1 one1 one1 one1 oNe1";
-        List<String> list = new ArrayList<>();
-        list.add(string1);
-        list.add(string2);
-
-        System.out.println(countWords(list));
-    }
-*/
-
-    public String countWords(List<String> lines) { //убрать статик
+    public String countWords(List<String> lines) throws UnsupportedOperationException {
         //throw new UnsupportedOperationException();
+
         ArrayList<String> wordsList = new ArrayList<>();
-        String separator = " ";
 
         // получаем список слов длиннее 4 символов
         for (String str : lines) {
 
-            String[] wordsArray = str.split(separator);
+            String[] wordsArray = str.replaceAll("[.,]", "").split(" ");
 
             for (String word : wordsArray) {
                 if (word.length() >= 4) wordsList.add(word);
             }
         }
 
-        //посчитать количество повторений каждого слова(нужны слова ≥10 раз)
-        HashSet<String> result = new HashSet<>();
+        //считаем количество повторений каждого слова(нужны слова ≥10 раз)
+        HashMap<String, Integer> result = new HashMap<>();
 
         for (int i = 0; i < wordsList.size(); i++) {
 
             int counter = 0;
+
             String checkWord = wordsList.get(i);
 
             for (String word : wordsList) {
@@ -49,10 +36,13 @@ public class Words {
             }
 
             if (counter >= 10) {
-                result.add(String.format("%s - %d", checkWord, counter));
+                result.put(checkWord, counter);
+                //result.add(String.format("%s - %d", checkWord, counter));
             }
         }
 
-        return result.toString();
+        return result.toString().replaceAll("=", " - ")
+                                .replaceAll(", ", "\n")
+                                .replaceAll("[{}]","");
     }
 }
